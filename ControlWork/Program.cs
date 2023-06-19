f205c6cfb32fe3b67a5d10d10fd4b604127fe5a2
Konstantin <kostik-kostik-12@mail.ru>
2023-06-19 21:09:56 +0300

    Оформил файл README.md описанием решения задачи

--- ControlWork/Program.cs
+++ ControlWork/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует 
+// новый массив из строк, длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. 
+
+string[] array1 = {"Hello", "World", "123", "2", "-2", "GB", "FinalWork"};
+string[] array2 = new string[array1.Length];
+void NewArrayMin3(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+NewArrayMin3(array1, array2);
+PrintArray(array2);
